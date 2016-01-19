968d81285525d2376460362a61910ad4a7e35bb0
Shahab Ali <shahabali1989@icloud.com>
2016-01-19 21:13:35 +0000

    added root route

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   get '/home', to: 'pages#home'
+
+  root 'pages#home'
   # Example resource route with options:
   #   resources :products do
   #     member do
