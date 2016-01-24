de46091dbc8dbf9b0af28d085061db719146301a
Shahab Ali <shahabali1989@icloud.com>
2016-01-24 21:33:33 +0000

    chef and recipe models editted

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,4 +1,5 @@
 class Chef < ActiveRecord::Base
+	has_many :recipes
 	validates :chefname, presence: true, length: {minimum: 3, maximum: 40}
 	VALID_EMAIL_REGEX = /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
 
