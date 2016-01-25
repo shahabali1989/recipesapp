b15f036d12cc97b5e26d499d451875fa215dbfa2
Shahab Ali <shahabali1989@icloud.com>
2016-01-25 12:27:04 +0000

    added styling and models

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,14 @@ Rails.application.routes.draw do
   get '/home', to: 'pages#home'
 
   root 'pages#home'
+
+  get '/recipes', to: 'recipes#index'
+  get '/recipes/new', to: 'recipes#new', as: 'new_recipe'
+  post '/recipes', to: 'recipes#create'
+  get 'recipes/:id/edit', to: 'recipes#edit', as: 'edit_recipe'
+  patch '/recipes/:id/', to: 'recipes#update'
+  get 'reciples/:id', to: 'recipes#show', as: 'recipe'
+  delete 'recipes/:id', to: 'recipes#destroy'
   # Example resource route with options:
   #   resources :products do
   #     member do
