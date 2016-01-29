3524e553d495e742829a6149159fd7d1e3d11097
Shahab Ali <shahabali1989@icloud.com>
2016-01-29 08:21:20 +0000

    added likes model and views

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,5 +1,6 @@
 class Chef < ActiveRecord::Base
 	has_many :recipes
+	has_many :likes
 	validates :chefname, presence: true, length: {minimum: 3, maximum: 40}
 	VALID_EMAIL_REGEX = /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
 
---
+++ app/models/like.rb
@@ -0,0 +1,7 @@
+class Like < ActiveRecord::Base
+
+belongs_to :chef
+belongs_to :recipe
+
+
+end
