c961ab91aee0af41c20d03716f7e8757fad8dc0e
Shahab Ali <shahabali1989@icloud.com>
2016-01-30 07:00:07 +0000

    added likes section and updates to model

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,7 @@
 class RecipesController < ApplicationController
 
 def index
-	@recipes = Recipe.all
+	@recipes = Recipe.all.sort_by{|likes| likes.thumbs_up_total}.reverse
 end
 
 def show
@@ -36,6 +36,21 @@ def update
 	else
 		render :edit
 	end
+end
+
+def like
+	@recipe = Recipe.find(params[:id])
+	newlike = Like.create(like: params[:like], chef: Chef.first, recipe: @recipe)
+	if newlike.valid?
+		if (newlike.like == true)
+			flash[:success] = "You liked this recipe!"
+		else
+			flash[:success] = "You disliked this recipe!"
+		end
+	else
+		flash[:danger] = "You can only like/dislike a recipe once"
+	end
+	redirect_to :back
 end
 
 private 
